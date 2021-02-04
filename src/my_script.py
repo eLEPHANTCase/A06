7cae9c1f7217d7e0969c67e243e2b988a8cf2ba0
Orville Dillon <60524259+eLEPHANTCase@users.noreply.github.com>
2021-02-03 22:41:50 -0500

    Task: Added Line to Dockerfile

---
+++ src/my_script.py
@@ -0,0 +1,7 @@
+#/usr/bin/python3
+# Copied from video
+from pystrich.datamatrix import DataMatrixEncoder
+
+encoder = DataMatrixEncoder("This is a DataMatrix.")
+encoder.save("/datamatrix_test.png")
+print(encoder.get_ascii())
